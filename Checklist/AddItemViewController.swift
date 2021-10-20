e09b19a8c4e8fc2210f6693725739ce143b77c41
Stepan Kirillov <stepakirillov59@gmail.com>
2021-10-20 21:07:38 +0500

    Created new window and added two buttons and Navigator Controller to it

---
+++ Checklist/AddItemViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AddItemViewController.swift
+//  Checklist
+//
+//  Created by Stepan Kirillov on 10/20/21.
+//
+
+import UIKit
+
+class AddItemViewController: UITableViewController {
+	
+	@IBAction func close() {
+		dismiss(animated: true, completion: nil)
+	}
+	
+	@IBAction func done() {
+		dismiss(animated: true, completion: nil)
+	}
+}
